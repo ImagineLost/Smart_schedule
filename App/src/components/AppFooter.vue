57042683f0b4bbe6a54aedf9e158c377eab69367
ImagineLost <dimasokolow22@gmail.com>
2026-05-14 19:55:07 +0300

    fixed everything now?

--- App/src/components/AppFooter.vue
+++ App/src/components/AppFooter.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <footer class="bg-light text-lg-start flex-column position-sticky bottom-0 z-3">
+    <footer class="bg-light text-center flex-column position-sticky bottom-0 z-3">
         <div class="p-3">
             © 2026 Smart Schedule. Made by Dmitry Sokolov. 
         </div>
