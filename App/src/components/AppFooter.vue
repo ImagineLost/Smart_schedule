ba97f4d8607d05e89e0798e539be73dbd493cadc
ImagineLost <dimasokolow22@gmail.com>
2026-05-14 20:01:06 +0300

    no contacts

--- App/src/components/AppFooter.vue
+++ App/src/components/AppFooter.vue
@@ -2,61 +2,12 @@
 </script>
 
 <template>
-    <footer class="bg text-center flex-column position-sticky bottom-0 z-3">
+    <footer class="bg-light text-center flex-column position-sticky bottom-0 z-3">
         <div class="p-3">
             © 2026 Smart Schedule. Made by Dmitry Sokolov. 
-        </div>
-        <div class="p-3">
-            <p>contact me:</p> 
-            <a href="https://t.me/Imagine_Lost">
-                <img src="../assets/img/telegram.png" alt="Telegram" class="socicon tg">
-            </a>
-            <a href="https://vk.com/imaginelost">
-                <img src="../assets/img/vk.png" alt="VK" class="socicon vk">
-            </a>
-            <a href="mailto:dimasokolow22@gmail.com">
-                <img src="../assets/img/email.png" alt="Email" class="socicon email">
-            </a>
-            <a href="https://github.com/ImagineLost">
-                <img src="../assets/img/github.png" alt="GitHub" class="socicon github">
-            </a>
-            <a href="https://www.youtube.com/@ImagineLost_OG">
-                <img src="../assets/img/youtube.png" alt="YouTube" class="socicon yt">
-            </a>
         </div>
     </footer>
 </template>
 
 <style>
-.bg {
-    background-color: #b1b1b1;
-}
-.socicon {
-    width: 70px;
-    height: 70px;
-    display: inline-block;
-    margin: 0 15px;
-    cursor: pointer;
-    transition: background-color 0.5s;
-}
-.vk:hover {
-    background-color: #0077ff;
-}
-
-.yt:hover {
-    background-color: #c30000;
-}
-
-.tg:hover {
-    background-color: #0088cc;
-}
-
-.email:hover {
-    background-color: #c74a10;
-    border: 1px solid #000;
-}
-.github:hover {
-    background-color: #333;
-}
-
 </style>
