e074f949a31277fe8c248568bbc4f1cfa0665645
ImagineLost <dimasokolow22@gmail.com>
2026-05-14 12:28:06 +0300

    Commentary centered

--- App/src/components/TodosDoneCheck.vue
+++ App/src/components/TodosDoneCheck.vue
@@ -12,13 +12,13 @@ export default {
       <h3>Выполненные задачи</h3>
       <span class="badge bg-success">Всего выполненных задач: {{ totalDone }}</span>
     </div>
-    <div v-if="totalDone === 0">
+    <div class="text-align-center" v-if="totalDone === 0">
       <p class="fw-bold">Пока нет выполненных задач. Очень жаль, но не сдавайтесь, продолжайте работать над своими целями!</p>
     </div>
-    <div v-if="totalDone <= 10 && totalDone > 0">
+    <div class="text-align-center" v-if="totalDone <= 10 && totalDone > 0">
       <p class="fw-bold">Отличная работа! Вы уже выполнили {{ totalDone }} задач. Продолжайте в том же духе, и вы достигнете величайших высот</p>
     </div>
-    <div v-if="totalDone > 10">
+    <div class="text-align-center" v-if="totalDone > 10">
       <p class="fw-bold">Вы выполнили {{ totalDone }} задач!!!!! Да вы просто бог мультитакскинга</p>
     </div>
   </div>
