cc85f4047c57a9b4bf304c2d9b8a4eae6e6082b7
ImagineLost <dimasokolow22@gmail.com>
2026-05-14 19:45:13 +0300

    contacts added

--- App/src/components/AppFooter.vue
+++ App/src/components/AppFooter.vue
@@ -2,12 +2,58 @@
 </script>
 
 <template>
-    <footer class="bg-light text-center text-lg-start position-sticky bottom-0 z-3">
-        <div class="text-center p-3">
-            © 2026 Smart Schedule
+    <footer class="bg-light text-lg-start flex-column position-sticky bottom-0 z-3">
+        <div class="p-3">
+            © 2026 Smart Schedule. Made by Dmitry Sokolov. 
+        </div>
+        <div class="p-3">
+            contact me: <a href="https://t.me/Imagine_Lost" class="socicon tg">
+                <img src="../assets/img/telegram.png" alt="Telegram" class="contact-icon">
+            </a>
+            <a href="https://vk.com/imaginelost" class="socicon vk">
+                <img src="../assets/img/vk.png" alt="VK" class="contact-icon">
+            </a>
+            <a href="mailto:dimasokolow22@gmail.com" class="socicon email">
+                <img src="../assets/img/email.png" alt="Email" class="contact-icon">
+            </a>
+            <a href="https://github.com/ImagineLost" class="socicon github">
+                <img src="../assets/img/github.png" alt="GitHub" class="contact-icon">
+            </a>
+            <a href="https://www.youtube.com/@ImagineLost_OG" class="socicon yt">
+                <img src="../assets/img/youtube.png" alt="YouTube" class="contact-icon">
+            </a>
         </div>
     </footer>
 </template>
 
 <style>
+.socicon {
+    width: 70px;
+    height: 70px;
+    border-radius: 100%;
+    display: inline-block;
+    margin: 0 15px;
+    cursor: pointer;
+    transition: background-color 0.5s;
+}
+.vk:hover {
+    background-color: #0077ff;
+}
+
+.yt:hover {
+    background-color: #c30000;
+}
+
+.tg:hover {
+    background-color: #0088cc;
+}
+
+.email:hover {
+    background-color: #fff;
+    border: 1px solid #000;
+}
+.github:hover {
+    background-color: #333;
+}
+
 </style>
